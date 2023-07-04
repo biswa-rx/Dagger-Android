838f30cee694483991ae383d7cb27aaf236def69
biswa <biswaranjanbeheraqazmlp@gmail.com>
2023-07-04 13:20:23 +0530

    Tut-4 Field Injection

--- app/src/main/java/com/example/dagge2_practice/MainActivity.kt
+++ app/src/main/java/com/example/dagge2_practice/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.dagge2_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var userRegistrationService: UserRegistrationService
+
+    @Inject
+    lateinit var emailService: EmailService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val component = DaggerUserRegistrationComponent.builder().build()
-        component.getUserRegistrationService().resisterUser("abc@gamil.com","123")
-        component.getEmailService().send("to","from","message")
+        component.inject(this)
+
+        userRegistrationService.resisterUser("Email","123")
     }
 }
--- app/src/main/java/com/example/dagge2_practice/UserRegistrationComponent.kt
+++ app/src/main/java/com/example/dagge2_practice/UserRegistrationComponent.kt
@@ -4,7 +4,5 @@ import dagger.Component
 
 @Component
 interface UserRegistrationComponent {
-    fun getUserRegistrationService() : UserRegistrationService
-
-    fun getEmailService() : EmailService
+    fun inject(mainActivity: MainActivity)
 }
